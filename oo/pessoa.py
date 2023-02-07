fa200d02142e1ea6dd9876c21452d20ee852bb49
Eduardo Sady <eduardosady21@gmail.com>
2023-02-07 20:55:57 -0300

    Criado os atributos de instancia, nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,18 @@
 class Pessoa:
-    def __init__(self, nome=None, idade = None): #Atributos de instancia e objetos são criados através dessa função.
+    def __init__(self, *filhos, nome=None, idade = 29): #Atributos de instancia e objetos são criados através dessa função.
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return 'Olá'
 
 if __name__ == '__main__':
-    p = Pessoa('Dudu', 29)
-    print (p.cumprimentar())
-    print (f'{p.cumprimentar()}, me chamo {p.nome} e tenho {p.idade} anos!')
+    mila = Pessoa(nome='Mila')
+    dudu = Pessoa(mila, nome='Dudu')
+    print (dudu.cumprimentar())
+
+    for filho in dudu.filhos:
+        print(filho.nome)
+
+    #print (f'{dudu.cumprimentar()}, me chamo {dudu.nome} e tenho {dudu.idade} anos!')
